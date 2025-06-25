45ffd85fd8710cabfe963438ae74ecd05ca3ed6c
DeViLs9966 <Minezmeylong@gmail.com>
2025-06-25 08:37:30 +0000

    Auto backup from check_bot_diagnostics

--- rita_main.py
+++ rita_main.py
@@ -942,3 +942,12 @@ import asyncio
         loop.run_until_complete(main())
     except (KeyboardInterrupt, SystemExit):
         pass
+
+
+if __name__ == '__main__':
+    import asyncio
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main())
+    except (KeyboardInterrupt, SystemExit):
+        pass
